6bc167689b5852359b56d3100604071f9724a603
chegBaba <17byhejunjie@gmail.com>
2019-04-17 11:50:13 +1200

    add queue for load; LEDBank

--- SOPC_files/software/FreqRelay/main.c
+++ SOPC_files/software/FreqRelay/main.c
@@ -28,6 +28,7 @@
 // #define Counter_Task_P      	(tskIDLE_PRIORITY)
 #define FreqAnalyser_Task_P		(tskIDLE_PRIORITY+5)
 #define LoadManager_Task_P 		(tskIDLE_PRIORITY+4)
+#define UpdateLED_Task_P 		(tskIDLE_PRIORITY+3)
 #define UpdateThresholds_P		(tskIDLE_PRIORITY+2)
 #define UpdateScreen_P			(tskIDLE_PRIORITY+1)
 /*Functions*/
@@ -54,12 +55,19 @@ struct FreqInfo
 preFreq{0,0},
 curFreq{0,0};
 
-/*Enum*/
+/*Enum and Constants*/
 enum NumLoads = 5;
 enum MaxRecordFreqs = 20;
 typedef enum{ MAINTAIN=0, RUN} SystemMode;
 typedef enum{true,false} bool;
-typedef enum{ON=0,OFF,SHED} LoadState;
+typedef enum{OFF=0,ON,SHED} LoadState;
+typedef enum{UNSTABLE,STABLE} SystemCondition;
+const unsigned int redMask = 0x1F;
+const unsigned int redL1Mask = 0x10;
+const unsigned int redL2Mask = 0x8;
+const unsigned int redL3Mask = 0x4;
+const unsigned int redL4Mask = 0x2;
+const unsigned int redL5Mask = 0x1;
 
 /*Threshold*/
 double Threshold_Freq = 50;
@@ -72,6 +80,8 @@ SystemMode currentSysMode = SystemMode::RUN;
 FreqInfo Freq_History[MaxRecordFreqs];
 int FreqHyIndex = 0;
 LoadState LoadBank[NumLoads] = {ON,ON,ON,ON,ON};
+SystemCondition currentSysStability = STABLE;
+unsigned int LEDBank = redMask;
 
 /*Timer*/
 TimeHandler_t timer_System;
@@ -80,19 +90,22 @@ TimeHandler_t timer_LoadManager;
 /*Queues:*/
 static QueueHandle_t Q_FreqInfo;
 static QueueHandle_t Q_KeyboardInput;
+static QueueHandle_t Q_LoadOperation;
 static QueueHandle_t Q_VGAUpdateValues;
 static QueueHandle_t Q_VGAUpdateTime;
 
 /*--------------------Inits--------------------*/
 void initSetupSystem()
-{
+{	
 	//Start with Five loads, indicate by RED LEDs
 	//TODO:if not boot with maintainence mode
-	IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, NumLoads);
+	if(currentSysMode != SystemMode::MAINTAIN)
+		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, LEDBank);
 
 	//Initiate Queues
 	Q_FreqInfo = xQueueCreate(100,sizeof(FreqInfo));
-	Q_KeyboardInput = xQueueCreate(100,sizeof(unsigned char));
+	Q_KeyboardInput = xQueueCreate(100,sizeof(SystemCondition));
+	Q_LoadOperation = xQueueCreate(100,sizeof(unsigned char));
 	Q_VGAUpdateValues = xQueueCreate(100,sizeof(unsigned char));//Change type
 	Q_VGAUpdateTime = xQueueCreate(100,sizeof(unsigned char));//change type
 }
@@ -137,6 +150,14 @@ void initCreateTask()
 			configMINIMAL_STACK_SIZE,
 			NULL,
 			FreqAnalyser_Task_P,
+			NULL );
+
+	xTaskCreate(
+			update_LED,
+			"updateLED_task",
+			configMINIMAL_STACK_SIZE,
+			NULL,
+			UpdateLED_Task_P,
 			NULL );
 	
 	//UpdateThresholds_P
@@ -230,41 +251,80 @@ void freq_analyser(void *pvParameters)
 	 * First check the current reading, if it's lower than threshold, call load ctr
 	 * If the avaliable data is more than 2, calculate RoC
 	 */
-	if (xQueueReceive( Q_FreqInfo, &curFreq, portMax_DELAY) == pdTRUE)
+	while (1)
 	{
-		if(curFreq.record_time == 0){//for first reading
-			preFreq = curFreq;
+		if (xQueueReceive( Q_FreqInfo, &curFreq, portMax_DELAY) == pdTRUE)
+		{
+			if(curFreq.record_time == 0){//for first reading
+				preFreq = curFreq;
+			}
+			RoC = abs(curFreq.freq_value - preFreq.freq_value)/abs(curFreq.record_time - preFreq.record_time);
 		}
-		RoC = abs(curFreq.freq_value - preFreq.freq_value)/abs(curFreq.record_time - preFreq.record_time);
-	}
-	/*Store readings to Freq_History[]*/
-	Freq_History[FreqHyIndex%MaxRecordFreqs] = curFreq;
-	
-	if(curFreq.freq_value > Threshold_Freq || RoC > Threshold_RoC)
-	{
-		//TODO: how to connect with load manager here?
-	}
-
-	
-
-
+		/*Store readings to Freq_History[]*/
+		Freq_History[FreqHyIndex%MaxRecordFreqs] = curFreq;
+		
+		if(curFreq.freq_value > Threshold_Freq || RoC > Threshold_RoC)
+		{
+			xQueueSendToBackFromISR(Q_LoadOperation,SystemCondition::UNSTABLE, pdFALSE);
+			currentSysStability = SystemCondition::UNSTABLE;
+		}else{
+			xQueueSendToBackFromISR(Q_LoadOperation,SystemCondition::STABLE, pdFALSE);
+			currentSysStability = SystemCondition::STABLE;
+		}
+	}	
 	//calculate the RoC
 	//TODO: implement system timer
-
-	//compare two thresholds
-
-	//call Load operations
+	
 }
 
 /* Load manager
- * Control the load operation
+ * Control the load operation,
+ * Read from Q_LoadOperation
  * ON,OFF,SHED
  */
 void load_manager(void *pvParameters)
 {
-	printf("Load manager currently empty")
-
+	printf("Load manager");
+	SystemCondition tempSysCon;
+	while(1)
+	{
+		if (xQueueReceive(Q_LoadOperation,&tempSysCon,portMax_DELAY) == pdTrue)
+		{
+			switch(tempSysCon){
+				case UNSTABLE: {
+					printf("Take one load off");
+					//TODO: need a timer here to count 500 ms before bring load backon?
+					break;
+				}
+				case STABLE: {
+					printf("Take one load back");					
+					break;
+				}
+			}
+			
+		}
+	}
+	printf("Load manager Error");
 }
+/* UpdateLED
+ * Read the switch value 
+ * Turn on the LED if system is stable
+ */
+void update_LED(void *pvParameters)
+{
+	while(currentSysStability)
+	{
+		//read switch value
+		uiSwitchValue = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE);
+		//For Most 5 right
+		LEDBank = LEDBank & uiSwitchValue; 
+
+		// write the value of the switches to the red LEDs
+		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, LEDBank);
+	}
+	printf("update_LED,system unstable, skipped");
+}
+
 
 int main()
 {
