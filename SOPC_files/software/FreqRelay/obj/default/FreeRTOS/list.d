65a2b3680939a52d76fd2ae28017a835502bd7d9
cheggJJH <17byhejunjie@gmail.com>
2019-04-22 01:30:56 +1200

    Add files via upload
    Need VGA plot feature

---
+++ SOPC_files/software/FreqRelay/obj/default/FreeRTOS/list.d
@@ -0,0 +1,36 @@
+obj/default/FreeRTOS/list.o: FreeRTOS/list.c FreeRTOS/FreeRTOS.h \
+ FreeRTOS/FreeRTOSConfig.h ../FreqRelay_bsp/system.h \
+ ../FreqRelay_bsp/linker.h FreeRTOS/projdefs.h FreeRTOS/portable.h \
+ FreeRTOS/deprecated_definitions.h FreeRTOS/portmacro.h \
+ ../FreqRelay_bsp//HAL/inc/sys/alt_irq.h \
+ ../FreqRelay_bsp//HAL/inc/nios2.h ../FreqRelay_bsp//HAL/inc/alt_types.h \
+ ../FreqRelay_bsp//HAL/inc/priv/alt_legacy_irq.h FreeRTOS/mpu_wrappers.h \
+ FreeRTOS/list.h
+
+FreeRTOS/FreeRTOS.h:
+
+FreeRTOS/FreeRTOSConfig.h:
+
+../FreqRelay_bsp/system.h:
+
+../FreqRelay_bsp/linker.h:
+
+FreeRTOS/projdefs.h:
+
+FreeRTOS/portable.h:
+
+FreeRTOS/deprecated_definitions.h:
+
+FreeRTOS/portmacro.h:
+
+../FreqRelay_bsp//HAL/inc/sys/alt_irq.h:
+
+../FreqRelay_bsp//HAL/inc/nios2.h:
+
+../FreqRelay_bsp//HAL/inc/alt_types.h:
+
+../FreqRelay_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+FreeRTOS/mpu_wrappers.h:
+
+FreeRTOS/list.h:
